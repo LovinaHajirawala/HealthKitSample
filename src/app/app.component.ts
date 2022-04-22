4f6087def9f0e6bcbf1d4ccdf46e059ec951f7b6
Lovina Hajirawala <LovinaH@LovinaH-MacBook-Pro.local>
2022-04-22 20:21:25 +0530

    poc closed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { HealthKit, HealthKitOptions } from '@ionic-native/health-kit/ngx';
-import { OpenNativeSettings } from '@awesome-cordova-plugins/open-native-settings/ngx';
-import { BackgroundFetch, BackgroundFetchConfig } from '@awesome-cordova-plugins/background-fetch/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   height: number;
   currentHeight = 'No Data';
   stepcount = 'No Data';
   heartRate = 0;
   workouts = [];
 
-  constructor(private healthKit: HealthKit,private openNativeSettings: OpenNativeSettings,private backgroundFetch: BackgroundFetch) {
-    // document.addEventListener('deviceReady', this.onDeviceReady(), false);
-  }
-  ngOnInit(): void {
-    // <key>NSHealthUpdateUsageDescription</key>
-    // <string>Read heart rate monitor data.</string>
-    // <key>NSHealthShareUsageDescription</key>
-    // <string>Share workout data with other apps.</string>
-   /*  const config: BackgroundFetchConfig = {
-      stopOnTerminate: false, // Set true to cease background-fetch from operating after user "closes" the app. Defaults to true.
-    }
-  
-    this.backgroundFetch.configure(config)
-       .then(() => {
-           console.log('Background Fetch initialized');
-  
-           this.test();
-  
-       })
-       .catch(e => console.log('Error initializing background fetch', e));
-  
-    // Start the background-fetch API. Your callbackFn provided to #configure will be executed each time a background-fetch event occurs. NOTE the #configure method automatically calls #start. You do not have to call this method after you #configure the plugin
-    this.backgroundFetch.start(); */
-  
-
-  
-    this.test();
+  constructor(
+    private healthKit: HealthKit,
+    private platform: Platform,
+    private zone: NgZone
+  ) {
+    platform.ready().then(() => {
+      this.platform.resume.subscribe(async () => {
+         this.test();
+      });
+    });
   }
 
-  test():void {
+   test() {
     this.healthKit.available().then((available) => {
-      console.log(available);
       if (available) {
-        // Request all permissions up front if you like to
-        var options: HealthKitOptions = {
+        const options: HealthKitOptions = {
           readTypes: [
             'HKQuantityTypeIdentifierHeight',
             'HKQuantityTypeIdentifierStepCount',
             'HKWorkoutTypeIdentifier',
             'HKQuantityTypeIdentifierActiveEnergyBurned',
             'HKQuantityTypeIdentifierDistanceCycling',
-            'HKQuantityTypeIdentifierHeartRate'
-          ]
+            'HKQuantityTypeIdentifierHeartRate',
+          ],
         };
         this.healthKit.requestAuthorization(options).then((_) => {
           this.loadHealthData();
@@ -66,8 +46,8 @@ export class AppComponent implements OnInit{
     });
   }
 
-  getheartData(){
-    var heartRateOptions = {
+  getheartData() {
+    const heartRateOptions = {
       startDate: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
       endDate: new Date(),
       sampleType: 'HKQuantityTypeIdentifierHeartRate',
@@ -76,18 +56,19 @@ export class AppComponent implements OnInit{
 
     this.healthKit.querySampleType(heartRateOptions).then(
       (data) => {
-        console.log("heart rate data",data);
-        
-        this.heartRate = data[0].quantity;
+        this.zone.run(() => {
+          console.log(data, "** data hearrt **")
+          this.heartRate = data[0].quantity;
+        });
       },
       (err) => {
-        console.log('No steps: ', err);
+        console.log('No heart: ', err);
       }
     );
   }
 
-  loadHealthData() {
-    var heartRateOptions = {
+  loadHealthData = () => {
+    const heartRateOptions = {
       startDate: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
       endDate: new Date(),
       sampleType: 'HKQuantityTypeIdentifierHeartRate',
@@ -95,14 +76,8 @@ export class AppComponent implements OnInit{
     };
 
     this.healthKit.monitorSampleType(heartRateOptions).then(
-      (data) => {
-        if(this.openNativeSettings){
-          
-       this.openNativeSettings.open('settings');
-        }
-        console.log("heart rate data",data);
+      () => {
         this.getheartData();
-
       },
       (err) => {
         console.log('No steps: ', err);
@@ -110,36 +85,32 @@ export class AppComponent implements OnInit{
     );
     this.healthKit.readHeight({ unit: 'cm' }).then(
       (val) => {
-        console.log(val);
-        this.currentHeight = val.value;
+        this.zone.run(() => {
+          this.currentHeight = val.value;
+        });
       },
       (err) => {
         console.log('No height: ', err);
       }
     );
-    
-    
-    var stepOptions = {
+
+    const stepOptions = {
       startDate: new Date(new Date().getTime() - 24 * 60 * 60 * 1000),
       endDate: new Date(),
       sampleType: 'HKQuantityTypeIdentifierStepCount',
       unit: 'count',
     };
-
-     this.healthKit.querySampleType(stepOptions).then(
+    console.log(stepOptions, "stepoptionss")
+    this.healthKit.querySampleType(stepOptions).then(
       (data) => {
-        let stepSum = data.reduce((a, b) => a + b.quantity, 0);
-        this.stepcount = stepSum;
+        this.zone.run(() => {
+          console.log(data, "** data steps **")
+          this.stepcount = data.reduce((a, b) => a + b.quantity, 0);
+        });
       },
       (err) => {
         console.log('No steps: ', err);
       }
     );
-
-   
-
-    
-  }
-
-
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy, Platform } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HealthKit } from '@ionic-native/health-kit/ngx';
-import { OpenNativeSettings } from '@awesome-cordova-plugins/open-native-settings/ngx';
-import { BackgroundFetch } from '@awesome-cordova-plugins/background-fetch/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, HealthKit,OpenNativeSettings,BackgroundFetch],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    HealthKit,
+    Platform,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
